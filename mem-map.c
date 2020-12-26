a5c592c6ce3985abc4effd850957232547c64bb4
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2020-12-26 12:51:38 +0000

    Memory matters

--- mem-map.c
+++ mem-map.c
@@ -5,12 +5,58 @@
 
 extern EFI_HANDLE LibImageHandle;
 
-static UINT64 base_mem_start = 0, base_mem_end = 0;
+/*
+ * Separately maintain
+ *   * available physical pages which can be accessed from real mode (up to
+ *     1 MiB)
+ *   * remaining available physical pages which can be accessed from 32-bit
+ *     protected mode (1 MiB--4 GiB)
+ *   * remaining available physical pages which can be accessed from 32-bit
+ *     protected mode (4 GiB on).
+ */
+static UINT64 mem20_start = 0, mem20_end = 0,
+	      mem32_start = 0, mem32_end = 0,
+	      mem64_start = 0, mem64_end = 0;
 
 static UINT64 carve_out_base_mem(unsigned kib)
 {
-	base_mem_end -= kib * 1024ULL;
-	return base_mem_end;
+	return mem20_end - 1024ULL;
+}
+
+static void record_mem_range(UINT64 start, UINT64 end)
+{
+	if (start < 0x100000ULL) {
+		if (end > 0x100000ULL) {
+			record_mem_range(start, 0x100000ULL);
+			record_mem_range(end, 0x100000ULL);
+		} else if (!mem20_end) {
+			mem20_start = start;
+			mem20_end = end;
+		} else if (start < mem20_start)
+			mem20_start = start;
+		  else if (end > mem20_end)
+			mem20_end = end;
+	} else if (start < 0x100000000ULL) {
+		if (end > 0x100000000ULL) {
+			record_mem_range(start, 0x100000000ULL);
+			record_mem_range(end, 0x100000000ULL);
+			return;
+		} else if (!mem32_end) {
+			mem32_start = start;
+			mem32_end = end;
+		} else if (start < mem32_start)
+			mem32_start = start;
+		  else if (end > mem32_end)
+			mem32_end = end;
+	} else {
+		if (!mem64_end) {
+			mem64_start = start;
+			mem64_end = end;
+		} else if (start < mem64_start)
+			mem64_start = start;
+		  else if (end > mem64_end)
+			mem64_end = end;
+	}
 }
 
 void mem_map_init(UINTN *mem_map_key)
@@ -25,9 +71,9 @@ void mem_map_init(UINTN *mem_map_key)
 	addr2 &= 0x000ffffffffff000ULL;
 	desc = LibMemoryMap(&num_entries, mem_map_key, &desc_sz, &desc_ver);
 	if (!num_entries)
-		error(u"cannot get memory map!");
+		error(u"cannot get mem. map!");
 	addr3 = (UINT64)desc;
-	cputws(u"memory map below 16 MiB:\n"
+	cputws(u"mem. map below 16 MiB:\n"
 		"  start    end       type attrs\n");
 	while (num_entries-- != 0) {
 		EFI_PHYSICAL_ADDRESS start = desc->PhysicalStart,
@@ -39,34 +85,34 @@ void mem_map_init(UINTN *mem_map_key)
 			ty2 = type;
 		if (start <= addr3 && addr3 < end)
 			ty3 = type;
-		if (start < 0xffffffUL) {
+		if (start < 0xffffffULL)
 			cwprintf(u"  0x%06lx 0x%06lx%c %4u 0x%016lx\n", start,
 			    end > 0x1000000ULL ? (UINT64)0x1000000ULL - 1
 					       : end - 1,
 			    end > 0x1000000ULL ? u'+' : u' ', type,
 			    desc->Attribute);
-			switch (type) {
-			    default:
-				break;
-			    case EfiBootServicesCode:
-			    case EfiBootServicesData:
-			    case EfiConventionalMemory:
-				if (start < 0xf0000ULL) {
-					if (end > 0xf0000ULL)
-						end = 0xf0000ULL;
-					if (!base_mem_end) {
-						base_mem_start = start;
-						base_mem_end = end;
-					} else if (end == base_mem_start)
-						base_mem_start = start;
-					  else if (start == base_mem_end)
-						base_mem_end = end;
-				}
-			}
+		switch (type) {
+		    default:
+			break;
+		    case EfiBootServicesCode:
+		    case EfiBootServicesData:
+		    case EfiConventionalMemory:
+		    case EfiACPIReclaimMemory:
+		    case EfiACPIMemoryNVS:
+			record_mem_range(start, end);
 		}
 		/* :-| */
 		desc = (EFI_MEMORY_DESCRIPTOR *)((char *)desc + desc_sz);
 	}
+	if (mem20_end)
+		cwprintf(u"available base mem. spans 0x%lx--0x%lx\n",
+		    mem20_start, mem20_end - 1);
+	if (mem32_end)
+		cwprintf(u"available 32-bit ext. mem. spans 0x%lx--0x%lx\n",
+		    mem32_start, mem32_end - 1);
+	if (mem64_end)
+		cwprintf(u"available 64-bit ext. mem. spans 0x%lx--0x%lx\n",
+		    mem64_start, mem64_end - 1);
 	if (ty1 != EfiMaxMemoryType)
 		cwprintf(u"loader stack mem. (@0x%lx) is type %u\n",
 		    addr1, ty1);
